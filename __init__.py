2bba21259a5fc7381fd6d713c17c326b3f8467fd
dj1394 <22375915@qq.com>
2026-09-07 21:26:50 +0800

    Initial commit: ComfyUI-DJ_align 节点对齐工具

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""大江对齐工具 - ComfyUI 节点对齐面板"""
+
+import os
+
+NODE_CLASS_MAPPINGS = {}
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+WEB_DIRECTORY = os.path.join(current_directory)
